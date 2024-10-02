385e883e83f30187e5c6d76cbbdc7e178f39b195
Maddie Wright <mwrigh07@student.ubc.ca>
2024-10-02 13:19:44 -0700

    Set up Test classes for Book and Library classes based on specification

---
+++ src/test/model/TestBook.java
@@ -0,0 +1,69 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestBook {
+    private Book testBook;
+    
+    @BeforeEach
+    void runBefore() {
+        testBook = new Book("Harry Potter", "J.K. Rowling", "Fantasy");
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals("Harry Potter", testBook.getTitle());
+        assertEquals("J.K. Rowling", testBook.getAuthor());
+        assertEquals("Fantasy", testBook.getGenre());
+        assertEquals("not started", testBook.getReadingStatus());
+        assertEquals(-1, testBook.getRating());
+        assertEquals(null, testBook.getReview());
+    }
+
+    // Test Setters/Getters
+
+    @Test
+    void testSetTitle() {
+        testBook.setTitle("The Silent Patient");
+        assertEquals("The Silent Patient", testBook.getTitle());
+    }
+
+    @Test
+    void testSetGenre() {
+        testBook.setGenre("Mystery");
+        assertEquals("Mystery", testBook.getGenre());   
+    }
+
+    @Test
+    void testSetAuthor() {
+        testBook.setAuthor("Alex Michaelides");
+        assertEquals("Alex Michaelides", testBook.getAuthor());
+    }
+
+    @Test
+    void testSetReadingStatus() {
+        testBook.setReadingStatus("in progress");
+        assertEquals("in progress", testBook.getReadingStatus());
+
+        testBook.setReadingStatus("completed");
+        assertEquals("completed", testBook.getReadingStatus());    
+    }
+
+    @Test
+    void testSetRating() {
+        testBook.setRating(1);
+        assertEquals(1, testBook.getRating()); 
+
+        testBook.setRating(4);
+        assertEquals(4, testBook.getRating());     
+    }
+
+    @Test
+    void testSetReview() {
+        testBook.setReview("This book is really good");
+        assertEquals("This book is really good", testBook.getReview()); 
+    }
+}
---
+++ src/test/model/TestLibrary.java
@@ -0,0 +1,70 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestLibrary {
+    private Library testLibrary;
+    private Book testBook;
+    private Book testBook2;
+
+
+    @BeforeEach
+    void runBefore() {
+        testLibrary = new Library();
+        testBook = new Book("Harry Potter", "J.K. Rowling", "Fantasy");
+        testBook2 = new Book("To Kill a Mockingbird", "Harper Lee", "Fiction");
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals(0, testLibrary.getBooks().size());
+    }
+
+    @Test
+    void testAddBook() {
+        testLibrary.addBook(testBook);
+        assertEquals(1, testLibrary.getBooks().size());
+        assertTrue(testLibrary.getBooks().contains(testBook));
+
+        testLibrary.addBook(testBook2);
+        assertEquals(2, testLibrary.getBooks().size());
+        assertTrue(testLibrary.getBooks().contains(testBook2));
+    }
+
+    @Test
+    void testRemoveBook() {
+        testLibrary.addBook(testBook);
+        testLibrary.addBook(testBook2);
+
+        testLibrary.removeBook(testBook);
+        assertEquals(1, testLibrary.getBooks().size());
+        assertFalse(testLibrary.getBooks().contains(testBook));
+        assertTrue(testLibrary.getBooks().contains(testBook2));
+    }
+
+    @Test
+    void testFindBookByGenre() {
+        testLibrary.addBook(testBook);
+        testLibrary.addBook(testBook2);
+
+        List<Book> fictionBooks = testLibrary.findBookByGenre("Fantasy");
+        assertTrue(fictionBooks.contains(testBook));
+        assertFalse(fictionBooks.contains(testBook2));
+    }
+
+    @Test
+    void testFindBookByAuthor() {
+        testLibrary.addBook(testBook);
+        testLibrary.addBook(testBook2);
+
+        List<Book> rowlingBooks = testLibrary.findBookByAuthor("J.K. Rowling");
+        assertTrue(rowlingBooks.contains(testBook));
+        assertFalse(rowlingBooks.contains(testBook2));
+
+    }
+}
