d1fbf332ba49ba25d4566e13a0bc5dff8e8dfe9b
Maddie Wright <mwrigh07@student.ubc.ca>
2024-10-04 18:25:17 -0700

    Added LibraryApp and Main classes with stubs and specifications

---
+++ src/main/ui/LibraryApp.java
@@ -0,0 +1,69 @@
+package ui;
+
+import model.Book;
+
+import java.util.Scanner;
+
+// Library application
+public class LibraryApp {
+    private Book book;
+    private Scanner input;
+
+    // EFFECTS: runs the library application
+    public LibraryApp() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: processes user input
+    public void runLibrary() {
+        // stub
+
+    }
+
+    // EFFECTS: displays menu of options to user
+    public void displayMenu() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: processes user command
+    private void processCommand(String command) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: 
+    private void newBook() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: 
+    private void updateBookStatus() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: 
+    private void doBookCompleted () {
+        // stub
+    }
+
+    // EFFECTS: 
+    private void doSearch() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: 
+    private void doEditbook() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: 
+    private void doRemoveBook() {
+
+    }
+}
