896f8683627417d204106e49ed790dea07a099b3
Maddie Wright <mwrigh07@student.ubc.ca>
2024-10-13 21:32:06 -0700

    Changed checkBook in TestJson to match Book contructor

--- src/test/persistance/TestJson.java
+++ src/test/persistance/TestJson.java
@@ -5,12 +5,12 @@ import model.Book;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestJson {
-    protected void checkBook(String title, String genre, String author,
-            int rating, String review, Book book) {
+    protected void checkBook(String title, String author, String genre, Book book) {
         assertEquals(title, book.getTitle());
         assertEquals(genre, book.getGenre());
         assertEquals(author, book.getAuthor());
-        assertEquals(rating, book.getRating());
-        assertEquals(review, book.getReview());
+        assertEquals("not started", book.getReadingStatus());
+        assertEquals(-1, book.getRating());
+        assertEquals("", book.getReview());
     }
 }
