6733c5d9cfcb7a7e46e02ca4c3932e8e6865e0bf
Maddie Wright <mwrigh07@student.ubc.ca>
2024-10-17 17:07:28 -0700

    Implemented JsonReader

--- src/main/persistance/JsonReader.java
+++ src/main/persistance/JsonReader.java
@@ -13,37 +13,65 @@ import org.json.*;
 
 // Represents a reader that reads library from JSON data stored in file
 public class JsonReader {
+    private String source;
 
     // EFFECTS: constructs reader to read from source file
     public JsonReader(String source) {
-        // stub
+        this.source = source;
     }
 
-     // EFFECTS: reads library from file and returns it;
+    // EFFECTS: reads library from file and returns it;
     // throws IOException if an error occurs reading data from file
     public Library read() throws IOException {
-        return null;
+        String jsonData = readFile(source);
+        JSONObject jsonObject = new JSONObject(jsonData);
+        return parseLibrary(jsonObject);
     }
 
     // EFFECTS: reads source file as string and returns it
     private String readFile(String source) throws IOException {
-        return null;
+        StringBuilder contentBuilder = new StringBuilder();
+
+        try (Stream<String> stream = Files.lines(Paths.get(source), StandardCharsets.UTF_8)) {
+            stream.forEach(s -> contentBuilder.append(s));
+        }
+
+        return contentBuilder.toString();
     }
 
     // EFFECTS: parses library from JSON object and returns it
     private Library parseLibrary(JSONObject jsonObject) {
-        return null;
+        // String name = jsonObject.getString("name");
+        Library lib = new Library();
+        addBooks(lib, jsonObject);
+        return lib;
     }
 
     // MODIFIES: lib
     // EFFECTS: parses books from JSON object and adds them to library
     private void addBooks(Library lib, JSONObject jsonObject) {
-        // stub
+        JSONArray jsonArray = jsonObject.getJSONArray("books");
+        for (Object json : jsonArray) {
+            JSONObject nextBook = (JSONObject) json;
+            addBook(lib, nextBook);
+        }
     }
 
     // MODIFIES: lib
     // EFFECTS: parses book from JSON object and adds it to library
     private void addBook(Library lib, JSONObject jsonObject) {
-        // stub
+        // String name = jsonObject.getString("name");
+        String title = jsonObject.getString("title");
+        String author = jsonObject.getString("author");
+        String genre = jsonObject.getString("genre");
+        String status = jsonObject.getString("status");
+        int rating = jsonObject.getInt("rating");
+        String review = jsonObject.getString("review");
+
+        Book book = new Book(title, author, genre);
+        book.setReadingStatus(status);
+        book.setRating(rating);
+        book.setReview(review);
+        lib.addBook(book);
     }
 }
