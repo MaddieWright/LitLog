39ae353244ac01bc1f2447677ed23dcd7b9a6b6b
Maddie Wright <mwrigh07@student.ubc.ca>
2024-10-29 20:10:23 -0700

    Added LibraryGUI class in ui package with specification of methods and fields

---
+++ src/main/ui/LibraryGUI.java
@@ -0,0 +1,75 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Main GUI for the Library app.
+ */
+public class LibraryGUI extends JFrame {
+
+    // Buttons for main menu actions
+    private JButton addBookButton;
+    private JButton viewBooksButton;
+    private JButton searchBooksButton;
+    private JButton editBooksButton;
+    private JButton saveButton;
+    private JButton loadButton;
+    private JButton quitButton;
+
+    // Display panel
+    private JPanel libraryPanel;
+
+    // Constructor sets up size, layout, and components.
+    public LibraryGUI() {
+
+    }
+
+    // Add header with title
+    private void addHeader() {
+
+    }
+
+    // Add welcome message and menu options on the left
+    private void addWelcomeAndMenu() {
+
+    }
+
+    // Add library section on the right to display list of books
+    private void addLibrarySection() {
+
+    }
+
+    // Add Save, Load, and Quit buttons at the bottom
+    private void addBottomButtons() {
+
+    }
+
+    // Method to open Add Book screen and include "Back" button
+    private void openAddBookScreen() {
+
+    }
+
+    // Method to open View Books screen and include "Back" button
+    private void openViewBooksScreen() {
+
+    }
+
+    // Method to open Search Books screen and include "Back" button
+    private void openSearchBooksScreen() {
+
+    }
+
+    // Method to open Edit Books screen and include "Back" button
+    private void openEditBooksScreen() {
+
+    }
+
+    // Method to return to main screen whne "Back" button is selected
+    private void returnToMainScreen() {
+
+    }
+
+}
